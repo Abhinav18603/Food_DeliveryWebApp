55bd5bfee9421ecbc6cc32871303c583f849a819
Abhinav Kumar <146194569+Abhinav18603@users.noreply.github.com>
2024-08-26 20:14:39 +0530

    Add files via upload

---
+++ FoodDelivery/frontend/src/assets/assets.js
@@ -0,0 +1,322 @@
+import basket_icon from './basket_icon.png'
+import logo from './logo.png'
+import header_img from './header_img.png'
+import search_icon from './search_icon.png'
+import menu_1 from './menu_1.png'
+import menu_2 from './menu_2.png'
+import menu_3 from './menu_3.png'
+import menu_4 from './menu_4.png'
+import menu_5 from './menu_5.png'
+import menu_6 from './menu_6.png'
+import menu_7 from './menu_7.png'
+import menu_8 from './menu_8.png'
+
+import food_1 from './food_1.png'
+import food_2 from './food_2.png'
+import food_3 from './food_3.png'
+import food_4 from './food_4.png'
+import food_5 from './food_5.png'
+import food_6 from './food_6.png'
+import food_7 from './food_7.png'
+import food_8 from './food_8.png'
+import food_9 from './food_9.png'
+import food_10 from './food_10.png'
+import food_11 from './food_11.png'
+import food_12 from './food_12.png'
+import food_13 from './food_13.png'
+import food_14 from './food_14.png'
+import food_15 from './food_15.png'
+import food_16 from './food_16.png'
+import food_17 from './food_17.png'
+import food_18 from './food_18.png'
+import food_19 from './food_19.png'
+import food_20 from './food_20.png'
+import food_21 from './food_21.png'
+import food_22 from './food_22.png'
+import food_23 from './food_23.png'
+import food_24 from './food_24.png'
+import food_25 from './food_25.png'
+import food_26 from './food_26.png'
+import food_27 from './food_27.png'
+import food_28 from './food_28.png'
+import food_29 from './food_29.png'
+import food_30 from './food_30.png'
+import food_31 from './food_31.png'
+import food_32 from './food_32.png'
+
+import add_icon_white from './add_icon_white.png'
+import add_icon_green from './add_icon_green.png'
+import remove_icon_red from './remove_icon_red.png'
+import app_store from './app_store.png'
+import play_store from './play_store.png'
+import linkedin_icon from './linkedin_icon.png'
+import facebook_icon from './facebook_icon.png'
+import twitter_icon from './twitter_icon.png'
+import cross_icon from './cross_icon.png'
+import selector_icon from './selector_icon.png'
+import rating_starts from './rating_starts.png'
+import profile_icon from './profile_icon.png'
+import bag_icon from './bag_icon.png'
+import logout_icon from './logout_icon.png'
+import parcel_icon from './parcel_icon.png'
+import checked from './checked.png'
+import un_checked from './un_checked.png'
+
+export const assets = {
+    logo,
+    basket_icon,
+    header_img,
+    search_icon,
+    rating_starts,
+    add_icon_green,
+    add_icon_white,
+    remove_icon_red,
+    app_store,
+    play_store,
+    linkedin_icon,
+    facebook_icon,
+    twitter_icon,
+    cross_icon,
+    selector_icon,
+    profile_icon,
+    logout_icon,
+    bag_icon,
+    parcel_icon,
+    checked,
+    un_checked
+}
+
+export const menu_list = [
+    {
+        menu_name: "Salad",
+        menu_image: menu_1
+    },
+    {
+        menu_name: "Rolls",
+        menu_image: menu_2
+    },
+    {
+        menu_name: "Deserts",
+        menu_image: menu_3
+    },
+    {
+        menu_name: "Sandwich",
+        menu_image: menu_4
+    },
+    {
+        menu_name: "Cake",
+        menu_image: menu_5
+    },
+    {
+        menu_name: "Pure Veg",
+        menu_image: menu_6
+    },
+    {
+        menu_name: "Pasta",
+        menu_image: menu_7
+    },
+    {
+        menu_name: "Noodles",
+        menu_image: menu_8
+    }]
+
+export const food_list = [
+    {
+        _id: "1",
+        name: "Salad",
+        image: food_1,
+        description: "A refreshing mix of crisp cucumbers, juicy tomatoes, crunchy bell peppers, red onions, olives, and feta cheese, all drizzled with a tangy lemon-oregano dressing.",
+        category: "Salad"
+    },
+    {
+        _id: "2",
+        name: "Veg salad",
+        image: food_2,
+        description: "A vibrant blend of fresh, crisp vegetables including lettuce, tomatoes, cucumbers, carrots, and bell peppers, tossed in a light, zesty vinaigrette.",
+        category: "Salad"
+    }, {
+        _id: "3",
+        name: "Clover Salad",
+        image: food_3,
+        description: "A refreshing mix of mixed greens, crunchy cucumbers, cherry tomatoes, and red onions, topped with clover sprouts and a tangy lemon vinaigrette.",
+        category: "Salad"
+    }, {
+        _id: "4",
+        name: "Chicken Salad",
+        image: food_4,
+        description: "Tender chicken pieces tossed with crisp greens, fresh vegetables, and a creamy dressing for a satisfying and hearty meal.",
+        category: "Salad"
+    }, {
+        _id: "5",
+        name: "Lasagna Rolls",
+        image: food_5,
+        description: "Savory rolls of pasta filled with rich cheese and hearty meat sauce, baked to perfection and topped with melted mozzarella.",
+        category: "Rolls"
+    }, {
+        _id: "6",
+        name: "Paneer Rolls",
+        image: food_6,
+        description: "Soft, warm flatbread wrapped around flavorful, spiced paneer with fresh vegetables, served with a tangy chutney.",
+        category: "Rolls"
+    }, {
+        _id: "7",
+        name: "Mutton Rolls",
+        image: food_7,
+        description: "A soft flatbread filled with tender, spiced mutton and fresh vegetables, served with a zesty sauce.",
+        category: "Rolls"
+    }, {
+        _id: "8",
+        name: "Paneer Cheese Rolls",
+        image: food_8,
+        description: "Grilled paneer and melted cheese wrapped in a soft flatbread with fresh veggies and tangy sauce.",
+        category: "Rolls"
+    }, {
+        _id: "9",
+        name: "Ripple Ice Cream",
+        image: food_9,
+        description: "Creamy vanilla ice cream swirled with luscious fruit ripple for a burst of flavor in every bite.",
+        category: "Deserts"
+    }, {
+        _id: "10",
+        name: "Fruit Ice Cream",
+        image: food_10,
+        description: "Creamy ice cream blended with a burst of fresh fruit flavors for a refreshing treat.",
+        category: "Deserts"
+    }, {
+        _id: "11",
+        name: "Jar Ice Cream",
+        image: food_11,
+        description: "Enjoy our creamy ice cream served in a whimsical jar, featuring a variety of delicious flavors and toppings. Each jar is a delightful treat, perfect for a fun and personalized dessert experience.",
+        category: "Deserts"
+    }, {
+        _id: "12",
+        name: "vanilla ice cream with colored sprinkles",
+        image: food_12,
+        description: "Sprinkles are made from corn syrup, sugar, cornstarch, wax, and artificial flavors and colors.",
+        category: "Deserts"
+    },
+    {
+        _id: "13",
+        name: "Veg Sandwich",
+        image: food_13,
+        description: "Fresh Veg Sandwich, packed with crisp vegetables, creamy avocado, and a hint of tangy dressing",
+        category: "Sandwich"
+    },
+    {
+        _id: "14",
+        name: "Vegan Sandwich",
+        image: food_14,
+        description: "A hearty, plant-based delight featuring fresh vegetables, avocado, and flavorful spreads, all nestled between slices of whole-grain bread. A satisfying and nutritious choice for any meal.",
+        category: "Sandwich"
+    }, {
+        _id: "15",
+        name: "Grilled Cheese Sandwich",
+        image: food_15,
+        description: "Melted cheddar and creamy mozzarella sandwiched between golden, crispy bread, creating a warm and gooey classic favorite.",
+        category: "Sandwich"
+    }, {
+        _id: "16",
+        name: "Lamb Sandwich",
+        image: food_16,
+        description: "Juicy slices of seasoned lamb with fresh veggies and a flavorful sauce, served on crusty bread.",
+        category: "Sandwich"
+    }, {
+        _id: "17",
+        name: "Cup Cake",
+        image: food_17,
+        description: "Strawberry flavoured cupcake",
+        category: "Cake"
+    }, {
+        _id: "18",
+        name: "Strawberry Cake",
+        image: food_18,
+        description: "A light and moist cake layered with luscious strawberry frosting, featuring fresh strawberry chunks for a burst of sweet, fruity flavor.",
+        category: "Cake"
+    }, {
+        _id: "19",
+        name: "Blueberry Vanilla Cake",
+        image: food_19,
+        description: " Vanilla Cake with Blueberries ",
+        category: "Cake"
+    }, {
+        _id: "20",
+        name: "Cheese Cake",
+        image: food_20,
+        description: "Cheese cake with vanilla filling",
+        category: "Cake"
+    }, {
+        _id: "21",
+        name: "Garlic Mushroom ",
+        image: food_21,
+        description: "Sautéed mushrooms infused with aromatic garlic and herbs, offering a savory and flavorful bite in every mouthful.",
+        category: "Pure Veg"
+    }, {
+        _id: "22",
+        name: "Golden Harvest Cauliflower & Corn",
+        image: food_22,
+        description: "Roasted cauliflower and sweet corn kernels, seasoned with a blend of herbs and spices for a flavorful and satisfying side dish.",
+        category: "Pure Veg"
+    }, {
+        _id: "23",
+        name: "Ceaser Salad",
+        image: food_23,
+        description: "Crisp romaine lettuce tossed with creamy Caesar dressing, crunchy croutons, and freshly grated Parmesan cheese.",
+        category: "Pure Veg"
+    }, {
+        _id: "24",
+        name: "Rice Zucchini",
+        image: food_24,
+        description: "Fluffy rice paired with tender zucchini, lightly seasoned for a fresh and flavorful dish.",
+        category: "Pure Veg"
+    },
+    {
+        _id: "25",
+        name: "Cheese Pasta",
+        image: food_25,
+        description: "Cheesy pasta tossed in a velvety cheese sauce, topped with melted cheese and a sprinkle of herbs.",
+        category: "Pasta"
+    },
+    {
+        _id: "26",
+        name: "Red Sauce Pasta",
+        image: food_26,
+        description: "Spicy red sauce pasta",
+        category: "Pasta"
+    }, {
+        _id: "27",
+        name: "Creamy Pasta",
+        image: food_27,
+        description: "Rich, velvety pasta coated in a creamy sauce, infused with garlic and Parmesan, for a luscious and comforting bite.",
+        category: "Pasta"
+    }, {
+        _id: "28",
+        name: "Chicken Pasta",
+        image: food_28,
+        description: "Tender chicken pieces tossed with al dente pasta in a creamy sauce, topped with fresh herbs and Parmesan.",
+        category: "Pasta"
+    }, {
+        _id: "29",
+        name: "Buttter Noodles",
+        image: food_29,
+        description: "Tender noodles tossed in rich, creamy butter, and lightly seasoned for a comforting dish.",
+        category: "Noodles"
+    }, {
+        _id: "30",
+        name: "Veg Noodles",
+        image: food_30,
+        description: "Spicy veg noodles",
+        category: "Noodles"
+    }, {
+        _id: "31",
+        name: "Somen Noodles",
+        image: food_31,
+        description: "Delicate somen noodles served cold, with a light dipping sauce for a refreshing bite.",
+        category: "Noodles"
+    }, {
+        _id: "32",
+        name: "Cooked Noodles",
+        image: food_32,
+        description: "Perfectly cooked noodles served hot, with a light seasoning for a comforting dish.",
+        category: "Noodles"
+    }
+]
